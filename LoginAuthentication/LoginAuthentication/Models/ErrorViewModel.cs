e8576fe51c3efb4048106cd9ffcb433608119d79
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-03-05 13:20:56 -0300

    Project Creation

---
+++ LoginAuthentication/LoginAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginAuthentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
